65d7f0e1cc662fc58ed80efffe7a17f58210bcea
Abel Lifaefi Mbula <abelmbula@protonmail.com>
2020-08-20 10:28:15 +0200

    render index.html plus add a logger

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const path = require("path");
+const logger = require("morgan");
 
 const app = express();
+
+app.use(logger("tiny"))
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
 module.exports = app;
