966490bf1031f66cc222d9dd6804b5bc78a9f7c7
cedric karungu <ckarungu921@gmail.com>
2022-07-04 08:45:56 +0200

    refact: update navbar

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const app = require("./app");
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
-    console.log(`The server is listning on port ${PORT}`)
+    console.log(`The server is listening on port ${PORT}`)
 );
