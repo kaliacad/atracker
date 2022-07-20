6ab10604809c6b9d6d95030315b757907f282156
cedric karungu <ckarungu921@gmail.com>
2022-07-20 12:06:19 +0200

    refact: add internal error page

--- src/utils/email/sendCoachEmail.js
+++ src/utils/email/sendCoachEmail.js
@@ -6,7 +6,6 @@ module.exports = async () => {
     const date = new Date().toISOString().split("T")[0];
     let usersEmail;
     let contentMail = `
-    <b>Équipe pédagogique GDA- status de présence</b>
     <p>
         En date du ${date} la situation des présences pour la classe dev GDA se présente comme suit :<br/>
         <ul>
@@ -42,8 +41,8 @@ module.exports = async () => {
     const mailOptions = {
         from: '"cedric karungu " <ckarungu921@kinshasadigital.com>', // sender address
         to: '"' + usersEmail + '", "ckarungu921@gmail.com"', // list of receivers
-        cc:'"jean-louis@kinshasadigital.com"',
-        subject: "Feadback des presences des apprenants ✔", // Subject line
+        cc: '"jean-louis@kinshasadigital.com"',
+        subject: "Équipe pédagogique GDA- status de présence ✔", // Subject line
         text: "Bonjour cher apprenant", // plain text body
         html: contentMail, // html body
     };
