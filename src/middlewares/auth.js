135516c57ba183ba120c3d85a9c99b99d893ca1a
cedric karungu <ckarungu921@gmail.com>
2022-11-29 20:06:45 +0200

    refact: update auth middleware logic

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line consistent-return
 export default (req, res, next) => {
     const userId = req.user ? req.user : undefined;
-    console.log(req.user);
     if (!userId) {
         // res.user.id = undefined;
         return res.redirect("/login");
